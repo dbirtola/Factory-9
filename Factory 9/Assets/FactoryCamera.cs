4ffa0501ec7f9400bb1be15439c4aaf36be3e680
Damen Birtola <dbirtola@ucsc.edu>
2018-02-08 15:17:08 -0700

    First commit

---
+++ Factory 9/Assets/FactoryCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FactoryCamera : MonoBehaviour {
+    public GameObject target;
+    public Vector2 offset;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        Debug.Log("ABAA");
+        Vector3 newPosition = new Vector3();
+        //Set x and y to the targets x and y
+        newPosition.x = target.transform.position.x + offset.x;
+        newPosition.y = target.transform.position.y + offset.y;
+        newPosition.z = transform.position.z; //Using the cameras z at all times.
+
+        transform.position = newPosition;
+    }
+}
---
+++ Factory 9/Assets/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+    Rigidbody2D rb;
+    Player player;
+
+    public bool canJump = true;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        player = GetComponent<Player>();
+    }
+
+
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            MoveHorizontal(player.speed * Time.deltaTime);
+
+        }else if (Input.GetKey(KeyCode.A))
+        {
+            MoveHorizontal(-1 * player.speed * Time.deltaTime);
+        }
+
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(canJump == true)
+            {
+                Jump();
+            }
+        }
+	}
+
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("Entered");
+        if (col.gameObject.layer == LayerMask.NameToLayer("Platform"))
+        {
+            canJump = true;
+        }
+    }
+
+
+
+    void MoveHorizontal(float speed)
+    {
+        //transform.position += Vector3.right * speed;
+        rb.AddForce(Vector2.right * speed);
+    }
+
+
+    void Jump()
+    {
+        canJump = false;
+        rb.AddForce(Vector2.up * player.jumpPower);
+    }
+
+
+
+
+}
