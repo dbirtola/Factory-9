4fc93b8c1a66b45b230cf14e7bef869160f263de
Damen Birtola <dbirtola@ucsc.edu>
2018-03-06 22:10:42 -0700

    Converter machine

---
+++ Factory 9/Assets/Ingredient.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum IngredientsEnum
+{
+    ExplosiveCrate,
+    Legs,
+    Warhead,
+    Arm,
+    MetalCyclinder
+}
+
+[System.Serializable]
+public class IngredientAmountPair
+{
+    public IngredientsEnum ingredient;
+    public int currentAmount = 0;
+    public int amountRequired;
+}
+
+
+public class Ingredient : MonoBehaviour {
+
+    public IngredientsEnum ingredient;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
