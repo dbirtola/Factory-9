5fa877f87800dfc62c1efe5f3429f6260a170355
ljmason <ljmason@ucsc.edu>
2018-03-07 01:19:38 -0800

    j

--- Factory 9/Assets/InvisibleTrigger.cs
+++ Factory 9/Assets/InvisibleTrigger.cs
@@ -7,19 +7,18 @@ public class InvisibleTrigger : Switch {
     // Use this for initialization
     private void OnTriggerEnter2D(Collider2D coll)
     {
-        Debug.Log("Trigger works");
         if (coll.gameObject.GetComponent<AIRobotController>() == null)
         {
             return;
         }
+        
         foreach (Activateable activatable in targetObjects)
             {
                 activatable.Activate();
-            }      
-    }
+            }
+        }
     private void OnTriggerExit2D(Collider2D coll)
     {
-        Debug.Log("Trigger works");
         if (coll.gameObject.GetComponent<AIRobotController>() == null)
         {
             return;
