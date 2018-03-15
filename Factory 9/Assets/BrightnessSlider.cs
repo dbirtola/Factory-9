3e6637b08ff1ef0896172f76d67adb06dfced76e
Damen Birtola <dbirtola@ucsc.edu>
2018-03-14 18:39:19 -0700

    k

---
+++ Factory 9/Assets/BrightnessSlider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BrightnessSlider : MonoBehaviour {
+
+    private Slider slider;
+    public Light directionalLight;
+	// Use this for initialization
+	void Start () {
+        slider = GetComponent<Slider>();
+        slider.value = directionalLight.intensity;
+        slider.onValueChanged.AddListener(valueChanged);
+	}
+
+    void valueChanged(float val)
+    {
+        directionalLight.intensity = val;
+    }
+
+}
