410b721d8aa242c38f47f9029b8293d23e09554a
dbirtola <dbirtola@gmail.com>
2019-04-15 17:07:30 -0600

    Cleaned up air vent entrance

---
+++ Factory 9/Assets/Scripts/Utility/AirVents/AirVent [Deprecated].cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Deprecated in favor of using AirVentEntrance. Originally intended to generate air vents programatically based on nodes placed throughout an area
+public class AirVent : MonoBehaviour {
+    
+    //Width of the generated air vents walls
+    public float width = 2f;
+
+    public BoxCollider2D ventWallPrefab;
+
+    public void GenerateCollider()
+    {
+
+        Debug.Log(transform.childCount);
+        //Do not use transform.childCount in loop, as the number will increase when we create walls
+        int childCount = transform.childCount;
+        for(int i = 0; i < childCount - 1; i+=2)
+        {
+            Transform child = transform.GetChild(i);
+            Transform child2 = transform.GetChild(i + 1);
+
+            //Create a wall on both sides of the child
+            CreateWall(child.position - child.right * width, child2.position - child2.right * width);
+            CreateWall(child.position + child.right * width, child2.position + child2.right * width);
+        }
+
+    }
+
+    void CreateWall(Vector2 point1, Vector2 point2)// Quaternion rotation)
+    {
+        //Find the midpoint
+        Vector2 position = (point1 + point2) / 2;
+
+        Debug.Log("instantiating");
+        BoxCollider2D wall = Instantiate(ventWallPrefab, position, Quaternion.identity);
+        wall.transform.up = point2 - point1;
+        Debug.Log("parenting");
+        wall.transform.SetParent(gameObject.transform, true);
+
+        Vector2 newSize = new Vector2(wall.size.x, Vector2.Distance(point1, point2));
+        wall.size = newSize;
+        
+    }
+
+}
---
+++ Factory 9/Assets/Scripts/Utility/AirVents/AirVentEntrance.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+/*Intended to be used to mark entrance and exits of air vents. No other scripts are needed, simply place the air vent wall prefabs to create
+ * a vent and ensure the an entrance script is on both sides so the players collision channels and air vent boolean are updated
+ */
+public class AirVentEntrance : MonoBehaviour {
+
+    //When the player enters an air vent entrance, we will switchm them over to "InAirVent" mode and change their collision channels so they can go through walls
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        //Only allow player, and only if he is in ball form
+        if (col.gameObject.GetComponent<Player>() == null || col.gameObject.GetComponent<Robot>().getNumberOfParts() > 0)
+            return;
+
+        col.gameObject.GetComponent<PlayerController>().playerRC.isInAirVent = true;
+
+        //Update the players collision channels to ignore walls
+        Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("Platform"));
+        Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Stealth"), LayerMask.NameToLayer("Platform"));
+
+    }
+
+
+    /*When the player leaves an air vent entrance, we check the angle to see if they are in the air vent or leaving the air vent.
+     * This assumes the designer has placed the air vent such that the local X axis is facing the exit
+     * 
+     */
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.GetComponent<Player>() == null)
+            return;
+
+
+
+        /*Compare air vent's x axis and the player's location to determine if the object exited the air vent or entered it. 
+         * Negative angles between the collider and player indicate leaving an air vent
+         */
+        Vector3 vectorToPlayer = (col.gameObject.transform.position - transform.position);
+        float angle = Vector2.SignedAngle(transform.right, vectorToPlayer);
+        if(angle < 0)
+        {
+            //Update collision masks so the player can collide with walls again
+            Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("Platform"), false);
+            Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Stealth"), LayerMask.NameToLayer("Platform"), false);
+            col.gameObject.GetComponent<PlayerController>().playerRC.isInAirVent = false;
+        }
+
+    }
+}
